4cb77c212b5de280e92e9ffe2d647fb167398ced
Neuroplastic1 <shafqat.umair@gmail.com>
2020-09-30 17:54:43 -0400

    Created Version 1  ImageForm

--- src/components/shared/ImageForm.js
+++ src/components/shared/ImageForm.js
@@ -1,2 +1,33 @@
 import React from 'react'
-import 
+import { Link } from 'react-router-dom'
+
+const ImageForm = ({ image, handleSubmit, handleChange, cancelPath }) => (
+  <form onSubmit={handleSubmit}>
+    <div>
+      <label>Image Tag</label>
+      <input
+        placeholder="Example: Tag names"
+        value={image.tag}
+        name="tag"
+        onChange={handleChange}
+      />
+    </div>
+    <br />
+    <div>
+      <label>Image url</label>
+      <input
+        placeholder="Example: Url of the image"
+        value={image.imageUrl}
+        name="url"
+        onChange={handleChange}
+      />
+    </div>
+    <br />
+    <button type="submit" className="btn btn-primary">Upload</button>
+     <Link to={cancelPath}>
+    <button className="btn btn-danger">Cancel</button>
+     </Link>
+  </form>
+  )
+
+  export default ImageForm
