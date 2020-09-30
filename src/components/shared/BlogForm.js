8c627015b19cf8e58adfd1a84f5924279e48a70f
Ashley Norwood <norwood54@icloud.com>
2020-09-30 17:59:58 -0400

    Fixed indentation on both forms Changed name value to "title" in BlogForm

--- src/components/shared/BlogForm.js
+++ src/components/shared/BlogForm.js
@@ -8,13 +8,13 @@ const BlogForm = ({ blog, handleSubmit, handleChange, cancelPath }) => (
       <input
         placeholder="Example: My thoughts"
         value={blog.title}
-        name="name"
+        name="title"
         onChange={handleChange}
       />
     </div>
     <br />
-      <button type="submit" className="btn btn-primary">Submit</button>
-        <Link to={cancelPath}>
+    <button type="submit" className="btn btn-primary">Submit</button>
+    <Link to={cancelPath}>
       <button className="btn btn-danger">Cancel</button>
     </Link>
   </form>
