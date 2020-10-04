c819a59c56497ceca10c8c3acd666733e97c160a
Christine <huangc909@gmail.com>
2020-10-04 12:48:42 -0400

    Completed the PostCreate Component Still need to test

---
+++ src/components/routes/Post.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+
+import apiUrl from '../../apiConfig'
+import PostForm from './../shared/PostForm'
+import messages from './../AutoDimissAlert/messages'
+
+const PostCreate = (props) => {
+  const [post, setPost] = useState({ tite: '', content: '' })
+  const [createdPostId, setCreatedPostId] = useState(null)
+  const handleChange = event => {
+    const updatedField = { [event.target.name]: event.target.value }
+
+    const editedPost = Object.assign({}, post, updatedField)
+
+    setPost(editedPost)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    const { msgAlert } = props
+    axios({
+      url: `${apiUrl}/blogs/${props.match.params.id}`,
+      method: 'POST',
+      headers: {
+        'Authorization': `Token token=${props.user.token}`
+      },
+      data: { post }
+    })
+      .then(res => {
+        const newPostId = res.data.blog.posts[res.data.blog.posts.length - 1]._id
+        return newPostId
+      })
+      .then(newPostId => setCreatedPostId(newPostId))
+      .then(() => msgAlert({
+        heading: 'Created post successfully',
+        message: messages.createPostSuccess,
+        variant: 'success'
+      }))
+      .catch(error => {
+        setPost({ tite: '', content: '' })
+        msgAlert({
+          heading: 'Create post failed: ' + error.message,
+          message: messages.createPostFailure,
+          variant: 'danger'
+        })
+      })
+  }
+  if (createdPostId) {
+    return <Redirect to={`/blogs/${props.match.params.id}`} />
+  }
+
+  return (
+    <div>
+      <PostForm
+        post={post}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        cancelPath={`/blogs/${props.match.params.id}`}
+      />
+    </div>
+  )
+}
+
+export default PostCreate
