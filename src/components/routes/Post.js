fc0e964ae14aecc2e9995ae561307ea5e1b7c3f0
Christine <huangc909@gmail.com>
2020-10-04 21:56:36 -0400

    Updated Post Component, but need GET to work

--- src/components/routes/Post.js
+++ src/components/routes/Post.js
@@ -1,1 +1,94 @@
 
+import React, { useState, useEffect } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import axios from 'axios'
+
+import apiUrl from '../../apiConfig'
+import messages from './../AutoDismissAlert/messages'
+
+const Post = props => {
+  const [post, setPost] = useState(null)
+  const [deleted, setDeleted] = useState(false)
+  const { msgAlert } = props
+
+  console.log(props)
+
+  useEffect(() => {
+    axios({
+      url: `${apiUrl}/blogs/${props.match.params.blogId}/posts/${props.match.params.postId}`,
+      method: 'GET',
+      headers: {
+        'Authorization': `Token token=${props.user.token}`
+      }
+    })
+      .then(res => {
+        console.log(res)
+        return res
+      })
+      .then(res => setPost(res.data.post))
+      .then(() => msgAlert({
+        heading: 'Showing selected post',
+        message: messages.showPostSuccess,
+        variant: 'primary'
+      }))
+      .catch(error => {
+        setPost({ title: '', content: '' })
+        msgAlert({
+          heading: 'Failed to show post' + error.message,
+          message: messages.showPostFailure,
+          variant: 'danger'
+        })
+      })
+  }, [])
+  const destroy = () => {
+    axios({
+      url: `${apiUrl}/blogs/${props.match.params.blogId}/posts/${props.match.params.postId}`,
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token token=${props.user.token}`
+      }
+    })
+      .then(() => setDeleted(true))
+      .then(() => msgAlert({
+        heading: 'Post Deleted',
+        message: messages.deletePostSuccess,
+        variant: 'success'
+      }))
+      .catch(error => {
+        setPost({ title: '', content: '' })
+        msgAlert({
+          heading: 'Failed to delete' + error.message,
+          message: messages.deletePostFailure,
+          variant: 'danger'
+        })
+      })
+  }
+
+  if (!post) {
+    return <p>Loading...</p>
+  }
+
+  if (deleted) {
+    return (
+      <Redirect to={`/blogs/${props.match.params.blogId}/posts`} />
+    )
+  }
+
+  return (
+    <div>
+      <h4>{post.title}</h4>
+      <p>{post.content}</p>
+      <div>
+        <button className="btn btn-danger" onClick={destroy}>Delete Post</button>
+        <Link to={`/blogs/${props.match.params.blogId}/posts/${props.match.params.postId}/edit-post`}>
+          <button className="button btn btn-warning">Edit Post</button>
+        </Link>
+      </div>
+      <div>
+        <Link to={`/blogs/${props.match.params.blogId}`}>Back to posts</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Post
