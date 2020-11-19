bc54fc22d1fc77f52e875f5c12b0afc401072849
Christine <huangc909@gmail.com>
2020-11-19 16:44:19 -0500

    ReWrote imageLike to have switch effect first Needed to update the state upon click even if on same page Next, I will work on bringing in the axios call

--- src/components/routes/ImageLike.js
+++ src/components/routes/ImageLike.js
@@ -1,125 +1,42 @@
 import React, { useState } from 'react'
 
-import axios from 'axios'
-
-import apiUrl from './../../apiConfig'
-
-import messages from './../AutoDismissAlert/messages'
-
 const ImageLike = props => {
   const [imageLike, setImageLike] = useState({
-    liked: true
+    liked: false
   })
-  const [deleted, setDeleted] = useState(false)
-  console.log(props)
-  const { image, msgAlert } = props
-  console.log('image in imageLike ', image)
+
+  // const [deleted, setDeleted] = useState(false)
+
+  const { image } = props
 
   const handleLike = image => {
-    console.log('handleLike with image argument! ', image)
-    console.log('user id ', props.user._id)
-    console.log('image owner id ', image.owner)
-    console.log(image.imageLikes)
-    const imageLikeOwners = image.imageLikes.map(el => el.owner)
-    console.log('imageLikeOwners ', imageLikeOwners)
-    const isImageLikeOwner = imageLikeOwners.includes(props.user._id)
-    console.log('isImageLikeOwner ', isImageLikeOwner)
-
-    // if true then direct to deleteLike
-    if (isImageLikeOwner) {
-      deleteLike(image)
-    } else {
-      // otherwise direct to createLike
-      createLike(image)
-    }
+    setImageLike((e) => { return { liked: !imageLike.liked } })
   }
+  console.log(imageLike)
 
-  const createLike = image => {
-    const newLike = Object.assign({}, imageLike)
-    console.log('newLike ', newLike)
-    setImageLike(newLike)
-    console.log('createLike imageLike ', imageLike)
-    console.log(image)
+  // const createLike = image => {
+  //   console.log('CREATE LIKE')
+  //   setImageLike((e) => { return { liked: true } })
+  //   setDeleted(false)
+  // }
+  // console.log('imageLike after createLike ', imageLike)
+  // console.log('deleted after createLike ', deleted)
+  //
+  // const deleteLike = image => {
+  //   console.log('DELETE LIKE')
+  //   setDeleted(true)
+  // }
+  // console.log('deleted after deleteLike ', deleted)
 
-    event.preventDefault()
-    axios({
-      url: `${apiUrl}/images/${image._id}/imageLikes`,
-      method: 'POST',
-      headers: {
-        'Authorization': `Token token=${props.user.token}`
-      },
-      data: { imageLike }
-    })
-      .then(res => { setImageLike(res.data.imageLike) })
-      .then(() => msgAlert({
-        heading: 'Image Liked',
-        message: messages.likeImageSuccess,
-        variant: 'primary'
-      }))
-      .catch(error => {
-        setImageLike([])
-        // message if images failed to show
-        msgAlert({
-          heading: 'Failed to delete' + error.message,
-          message: messages.likeImageFailure,
-          variant: 'danger'
-        })
-      })
-  }
-
-  const deleteLike = image => {
-    console.log('DeleteLike!')
-
-    // map through imageLikes to find all owners
-    const imageLikeOwners = image.imageLikes.map(el => el.owner)
-    console.log('imageLikeOwners ', imageLikeOwners)
-
-    // find the index of the owner that matches the user's id
-    const imageLikeOwnerIndex = imageLikeOwners.indexOf(props.user._id)
-    console.log('imageLikeOwnerIndex ', imageLikeOwnerIndex)
-
-    // get the id of the imageLike owner
-    const imageLikeOwnerId = image.imageLikes[imageLikeOwnerIndex]._id
-    console.log('imageLikeOwnerId ', imageLikeOwnerId)
-
-    axios({
-      url: `${apiUrl}/images/${image._id}/imageLikes/${imageLikeOwnerId}`,
-      method: 'DELETE',
-      headers: {
-        'Authorization': `Token token=${props.user.token}`
-      }
-    })
-      .then(() => setDeleted(true))
-      .then(() => msgAlert({
-        heading: 'Message Unliked',
-        message: messages.unlikeImageSuccess,
-        variant: 'primary'
-      }))
-      .catch(error => {
-        setImageLike([])
-        // message if images failed to show
-        msgAlert({
-          heading: 'Failed to delete' + error.message,
-          message: messages.unlikeImageFailure,
-          variant: 'danger'
-        })
-      })
-    console.log(imageLike)
-  }
-
-  if (deleted) {
-    console.log('deleted!')
-  }
-
+  const likeIcon = imageLike.liked ? './../../images/like-icon.png' : './../../images/unlike-icon.png'
   return (
     <img
       key={image._id}
       className='like-icon'
-      src={'./../../images/unlike-icon.png'}
+      src={likeIcon}
       style={{ cursor: 'pointer' }}
       onClick={() => {
         handleLike(image)
-        // handleSubmit(event)
       }}
     />
   )
