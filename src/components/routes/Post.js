e1d66c5afa15849de42d3ef6851e5735be14a4b2
Christine <huangc909@gmail.com>
2020-11-30 09:01:51 -0500

    Updated Comments to display commenter, instead of user Need to work on redirect link after comment create and post create

--- src/components/routes/Post.js
+++ src/components/routes/Post.js
@@ -74,10 +74,11 @@ const Post = props => {
       <Redirect to={`/blogs/${props.match.params.blogId}`} />
     )
   }
+  console.log(post)
 
   const commentsJsx = post.comments.map(comment => (
     <li className="comment-list" key={comment._id}>
-      <p>Posted By: {props.user._id}</p>
+      <p>Posted By: {comment.commenter}</p>
       <p>{comment.remark}</p>
     </li>
   ))
