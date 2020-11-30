4c7d26e4e669e6fea40393f2f524976ee9021061
Christine <huangc909@gmail.com>
2020-11-30 07:14:48 -0500

    Changed path to homepage upon sign up

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -35,7 +35,7 @@ class SignUp extends Component {
         message: messages.signUpSuccess,
         variant: 'success'
       }))
-      .then(() => history.push('/'))
+      .then(() => history.push('/home-page'))
       .catch(error => {
         this.setState({ email: '', password: '', passwordConfirmation: '' })
         msgAlert({
