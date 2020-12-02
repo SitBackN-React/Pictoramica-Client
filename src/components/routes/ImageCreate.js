21bc4801005bd1951dd9c2f5e2b8af67e67bb225
Ashley Norwood <norwood54@icloud.com>
2020-12-02 18:26:30 -0500

    Added image formatting: Changed redirect route after comment create is successful, right now it goes to the public version of the post that was commented on. *this will not stay this way*,will make new routes for comment public/private

--- src/components/routes/ImageCreate.js
+++ src/components/routes/ImageCreate.js
@@ -1,60 +1,22 @@
 import React, { useState } from 'react'
-import { Redirect, Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 import apiUrl from '../../apiConfig'
-// import ImageForm from './../shared/ImageForm'
+import ImageForm from './../shared/ImageForm'
 import messages from './../AutoDismissAlert/messages'
-
-import Upload from './Upload'
 
 const ImageCreate = (props) => {
   // state of the image starts as empty string, 0, or false
   const [image, setImage] = useState({ tag: '', caption: '', imageUrl: '', like: 0, forSale: false })
   // state starts as null, will be updated once image created
   const [createdImageId, setCreatedImageId] = useState(null)
-
-  const [selectedFiles, setSelectedFiles] = useState(undefined)
-  const [currentFile, setCurrentFile] = useState(undefined)
-  const [progress, setProgress] = useState(0)
-  const [message, setMessage] = useState('')
-  const [setFileInfos] = useState([])
-
   // uses handle change and passes in the event param to handle action item
   const handleChange = event => {
     const updatedField = { [event.target.name]: event.target.value }
     // sets the newly created image to an object
     const editedImage = Object.assign({}, image, updatedField)
     setImage(editedImage)
-  }
-
-  const selectFile = (event) => {
-    setSelectedFiles(event.target.files)
-  }
-
-  const upload = () => {
-    const currentFile = selectedFiles[0]
-
-    setProgress(0)
-    setCurrentFile(currentFile)
-
-    Upload.upload(currentFile, (event) => {
-      setProgress(Math.round((100 * event.loaded) / event.total))
-    })
-      .then((response) => {
-        setMessage(response.data.message)
-        return Upload.getFiles()
-      })
-      .then((files) => {
-        setFileInfos(files.data)
-      })
-      .catch(() => {
-        setProgress(0)
-        setMessage('Could not upload the file!')
-        setCurrentFile(undefined)
-      })
-
-    setSelectedFiles(undefined)
   }
   // submits the change
   const handleSubmit = event => {
@@ -95,63 +57,12 @@ const ImageCreate = (props) => {
   // cancel path takes user back to all image page.
   return (
     <div>
-      {currentFile && (
-        <div className="progress">
-          <div
-            className="progress-bar progress-bar-info progress-bar-striped"
-            role="progressbar"
-            aria-valuenow={progress}
-            aria-valuemin="0"
-            aria-valuemax="100"
-            style={{ width: progress + '%' }}
-          >
-            {progress}%
-          </div>
-        </div>
-      )}
-
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Image Caption</label>
-          <input
-            type="text"
-            placeholder="Example: My sunset painting"
-            value={image.caption}
-            name="caption"
-            size="25"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Image Tag</label>
-          <input
-            type="text"
-            placeholder="Example: #sunset #painting"
-            value={image.tag}
-            name="tag"
-            size="25"
-            onChange={handleChange}
-          />
-        </div>
-        <br />
-        <label className="btn btn-default">Image Url</label>
-        <input type="file" onChange={selectFile} />
-        <br />
-        <button
-          className="btn btn-success"
-          disabled={!selectedFiles}
-          onClick={upload}
-        >
-          Upload
-        </button>
-
-        <div className="alert alert-light" role="alert">
-          {message}
-        </div>
-        <Link to='/my-images'>
-          <button className="btn btn-danger">Cancel</button>
-        </Link>
-      </form>
+      <ImageForm
+        image={image}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        cancelPath='/images'
+      />
     </div>
   )
 }
--- src/components/routes/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import apiUrl from '../../apiConfig'
-
-export default axios.create({
-  baseURL: apiUrl,
-  headers: {
-    'Content-type': 'application/json'
-  }
-})
