2df4c1a410d15bff0992fcb97c5c0e2758c310cd
Christine <huangc909@gmail.com>
2021-02-10 19:02:53 -0500

    Changed code back to original code

--- src/components/routes/ImageLike.js
+++ src/components/routes/ImageLike.js
@@ -1,55 +1,80 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
 import messages from './../AutoDismissAlert/messages'
 
 const ImageLike = props => {
-  const { image, msgAlert } = props
+  const { image, msgAlert, user } = props
 
-  const [imageLikeArr, setImageLikeArr] = useState(null)
+  const imageLikeOwners = image.imageLikes.map(el => el.owner)
 
-  useEffect(() => {
+  const isImageLikeOwner = imageLikeOwners.includes(user._id)
+
+  const [userLiked, setUserLiked] = useState(isImageLikeOwner)
+
+  const handleLike = image => {
+    userLiked ? deleteLike(image) : createLike(image)
+  }
+
+  const createLike = image => {
+    event.preventDefault()
     axios({
-      url: `${apiUrl}/imageLikes`,
-      method: 'GET'
+      url: `${apiUrl}/images/${image._id}/imageLikes`,
+      method: 'POST',
+      headers: {
+        'Authorization': `Token token=${user.token}`
+      },
+      data: { imageLike: {
+        liked: true
+      } }
     })
-      .then(res => setImageLikeArr(res.data.imageLikes))
+      .then(res => {
+        // console.log(res)
+        return res
+      })
+      .then((e) => setUserLiked(true))
       .then(() => msgAlert({
-        heading: 'Showing all imageLikes',
-        message: messages.showAllImageLikesSuccess,
+        heading: 'Image Liked',
+        message: messages.likeImageSuccess,
         variant: 'primary'
       }))
       .catch(error => {
-        setImageLikeArr([])
         // message if images failed to show
         msgAlert({
           heading: 'Failed to delete' + error.message,
-          message: messages.showAllImageLikesFailure,
+          message: messages.likeImageFailure,
           variant: 'danger'
         })
       })
-  }, [])
-  console.log('imageLikeArr: ', imageLikeArr) // null until completed GET request
+  }
+  // console.log('after create ', userLiked)
 
-  // if (imageLikeArr !== null) {
-  //   console.log('imageLikeArr[0]: ', imageLikeArr[0])
-  //   console.log('imageLikeArr[0].imageId: ', imageLikeArr[0].imageId)
-  //   console.log('image._id: ', image._id)
-  //   const specOwner = imageLikeArr.filter(imageLike => imageLike.imageId === image._id).filter(owner => owner.owner === user._id)
-  //   // const specOwner = specImage.filter(owner => owner.owner === user._id)
-  //   console.log('Specific Owner Array: ', specOwner) // this shows only if ids match; otherwise it would be an empty array
-  //   if (specOwner !== undefined) { // tried to input a conditional, but it still considers undefined data
-  //     console.log('First Specific Owner: ', specOwner[0]) // if there is no specific owner array then this becomes undefined
-  //   }
-  //   // console.log(specOwner[0].liked)
-  // }
+  const deleteLike = image => {
+    const imageLikeOwnerIndex = imageLikeOwners.indexOf(user._id)
 
-  const [userLiked] = useState(false)
-
-  const handleLike = () => {
-    console.log('handleLike')
-    // change heart color
+    const imageLikeId = image.imageLikes[imageLikeOwnerIndex]._id
+    axios({
+      url: `${apiUrl}/images/${image._id}/imageLikes/${imageLikeId}`,
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token token=${user.token}`
+      }
+    })
+      .then((e) => setUserLiked(false))
+      .then(() => msgAlert({
+        heading: 'Message Unliked',
+        message: messages.unlikeImageSuccess,
+        variant: 'primary'
+      }))
+      .catch(error => {
+        // message if images failed to show
+        msgAlert({
+          heading: 'Failed to delete' + error.message,
+          message: messages.unlikeImageFailure,
+          variant: 'danger'
+        })
+      })
   }
 
   const likeIcon = userLiked ? './../../images/like-icon.png' : './../../images/unlike-icon.png'
@@ -64,7 +89,7 @@ const ImageLike = props => {
           handleLike(image)
         }}
       />
-      <div></div>
+      <p>{image.imageLikes.length}</p>
     </div>
   )
 }
