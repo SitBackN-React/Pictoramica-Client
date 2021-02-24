204e09a5a99d6bffd63a4c3259a1821354f46f01
Christine <huangc909@gmail.com>
2021-02-24 10:35:38 -0500

    Updated AllImages so that the imageLike button can be displayed and used in AllImages component

--- src/components/routes/AllImages.js
+++ src/components/routes/AllImages.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 import apiUrl from './../../apiConfig'
-// import ImageLike from './ImageLike'
+import ImageLike from './ImageLike'
 import ForSale from './ForSale'
 import ProductDisplay from './../App/Checkout'
 
@@ -60,6 +60,42 @@ const AllImages = (props) => {
     imageBox: { width: '60%', height: '60%', border: '2px solid #000000' }
   }
 
+  // Checks to see if the user has a imageLike or not in the image
+  const checkUserLike = image => {
+    if (image.imageLikes.length === 0) {
+      return false
+    } else {
+      const findImageLike = image.imageLikes.filter(imageLike => imageLike.owner === props.user._id)
+      if (findImageLike) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+
+  // Looks for the imageLike id in the image
+  // if there is one that the user created, return that 'id'
+  // if not, return '0'
+  const imageLikedId = image => {
+    if (image.imageLikes.length === 0) {
+      return '0'
+    } else {
+      const findImageLike = image.imageLikes.filter(imageLike => imageLike.owner === props.user._id)
+      if (findImageLike) {
+        const imageLikeId = findImageLike[0]._id
+        return imageLikeId
+      } else {
+        return '0'
+      }
+    }
+  }
+
+  // Determines how many imageLikes there are in total for each image
+  const imageLikedCount = image => {
+    return image.imageLikes.length
+  }
+
   const imagesJsx = allImages.map(image =>
     <div key={image._id}>
       <div style={styles.imageBox}>
@@ -79,6 +115,14 @@ const AllImages = (props) => {
             user={props.user}
           />
         </div>
+        <ImageLike
+          image={image}
+          userLiked={checkUserLike(image)}
+          imageLikedId={imageLikedId(image)}
+          imageLikedCount={imageLikedCount(image)}
+          {...props}
+          user={props.user}
+        />
         <ProductDisplay
           image={image}
           {...props}
